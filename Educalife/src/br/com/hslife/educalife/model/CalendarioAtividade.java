abe1cb6b60d85325e4c34a882fa72f2701fc1185
Hércules S. S. José <hercules_hssj@outlook.com>
2020-08-27 11:59:53 -0300

    Caso de Uso CalendarioAtividade

---
+++ Educalife/src/br/com/hslife/educalife/model/CalendarioAtividade.java
@@ -0,0 +1,50 @@
+package br.com.hslife.educalife.model;
+
+import java.time.*;
+
+import javax.persistence.*;
+
+import org.hibernate.envers.*;
+import org.openxava.annotations.*;
+import org.openxava.model.*;
+
+@Entity
+@Table(name="calendario_atividade")
+@Audited
+public class CalendarioAtividade extends Identifiable {
+	
+	@Column(nullable = false)
+	@Required
+	private String descricao;
+	
+	@Column(nullable = false)
+	@Required
+	private LocalDate dataInicio;
+	
+	@Column(nullable = true)
+	private LocalDate dataFim;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public LocalDate getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(LocalDate dataFim) {
+		this.dataFim = dataFim;
+	}
+}
