d1505529b9ff04131fe4dda6f88d8aa5ba42e88f
Hércules S. S. José <hercules@tic.ufrj.br>
2020-12-13 01:17:33 -0300

    Teste com o builder genérico.

--- Educalife/src/br/com/hslife/educalife/schedule/PopulaBase.java
+++ Educalife/src/br/com/hslife/educalife/schedule/PopulaBase.java
@@ -1,11 +1,45 @@
 package br.com.hslife.educalife.schedule;
 
+import java.util.Random;
+import java.util.UUID;
+
+import org.openxava.jpa.XPersistence;
 import org.springframework.stereotype.*;
+
+import br.com.hslife.educalife.model.Cargo;
+import br.com.hslife.educalife.patterns.Builder;
 
 @Component("populaBaseService")
 public class PopulaBase {
 
 	public void populaBase() {
+
+		populaCargo();
+		
 		System.out.println("Base populada com sucesso!");
+	}
+
+	private void populaCargo() {
+		try {
+
+			// Popula a tabela de cargo
+			Builder<Cargo> cargoBuilder = new Builder<>(new Cargo());
+			Cargo cargo = cargoBuilder.set("codigoCbo", String.valueOf(new Random().nextInt(999999)))
+				.set("denominacao", this.geraTexto("Cargo "))
+				.set("ativo", true)
+				.build();
+
+			XPersistence.getManager().persist(cargo);
+			XPersistence.commit();
+
+		} catch (Exception e) {
+			XPersistence.rollback();
+			e.printStackTrace();
+		}
+	}
+
+	private String geraTexto(String texto) {
+		String textoGerado = texto + UUID.randomUUID().toString().substring(0, 8);
+		return textoGerado;
 	}
 }
