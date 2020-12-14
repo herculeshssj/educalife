1d16efa72b8a4e59d8e5d8e740a380b95a7f5d97
Hércules S. S. José <hercules@tic.ufrj.br>
2020-12-14 11:13:13 -0300

    Trazendo de volta os dados de teste.

---
+++ Educalife/Scripts/rascunhoFrequencia.sql
@@ -0,0 +1,26 @@
+select * from frequencia f ;
+
+select * from inscricao_turma_frequencia itf ;
+
+select * from inscricao_turma it ;
+
+alter table frequencia add id_inscricao_turma varchar(255);
+alter table frequencia drop column id_inscricao_turma;
+
+
+select id, id_inscricao_turma from frequencia f where f.id_inscricao_turma not in (select distinct inscricao_turma_id from inscricao_turma_frequencia);
+select distinct inscricao_turma_id from inscricao_turma_frequencia;
+
+insert into inscricao_turma_frequencia (inscricao_turma_id, frequencia_id)
+	select  
+		f.id_inscricao_turma,
+		f.id 
+	from 
+		frequencia f 
+	where 
+		f.id_inscricao_turma not in (
+			select distinct inscricao_turma_id from inscricao_turma_frequencia
+		); 
+	
+	
+	
