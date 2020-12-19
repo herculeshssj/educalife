ace78bcff40edc768af115c518e3d4c630060871
Hércules S. S. José <hercules@tic.ufrj.br>
2020-12-19 10:02:32 -0300

    Salvando o rascunho

--- Educalife/Scripts/rascunhoProcesso.sql
+++ Educalife/Scripts/rascunhoProcesso.sql
@@ -1,3 +1,16 @@
 select * from tipo_documento td ;
 
 select * from tipo_processo tp ;
+
+select * from processo p ;
+
+select * from empresa e ;
+
+select count(*) from processo p 
+'inner join departamento d on d.id = p.id_departamento 
+inner join unidade u on u.id = d.id_unidade 
+inner join empresa e on e.id = u.id_empresa' 
+where datepart(year, data_abertura) = datepart(year, getdate())
+and e.id = '2c9e80817638473201763856e7ff0006'; 
+
+
