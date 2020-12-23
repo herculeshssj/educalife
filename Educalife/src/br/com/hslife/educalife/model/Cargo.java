3bac8561503cd13d7445aea3c6860780ebd2bb14
Hércules S. S. José <hercules@tic.ufrj.br>
2020-12-23 13:24:48 -0300

    Teste com as anotações pré-gravação na base de dados.

--- Educalife/src/br/com/hslife/educalife/model/Cargo.java
+++ Educalife/src/br/com/hslife/educalife/model/Cargo.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 import org.hibernate.envers.*;
 import org.openxava.annotations.*;
 import org.openxava.model.*;
+import org.openxava.util.Users;
 
 @Entity
 @Table(name="cargo")
@@ -22,6 +23,36 @@ public class Cargo extends Identifiable {
 	@Column
 	private boolean ativo;
 
+	@PreCreate
+	public void antesDeCriar() {
+		if (Users.getCurrent() != null) 
+			System.out.println("*** Método executado antes de criar a entidade ***");
+	}
+
+	@PrePersist
+	public void antesDePersistir() {
+		if (Users.getCurrent() != null) 
+			System.out.println("*** Método executado antes de persistir a entidade ***");
+	}
+
+	@PreUpdate
+	public void antesDeAtualizar() {
+		if (Users.getCurrent() != null) 
+			System.out.println("*** Método executado antes de atualizar a entidade ***");
+	}
+
+	@PreDelete
+	public void antesDeExcluir() {
+		if (Users.getCurrent() != null) 
+			System.out.println("*** Método executado antes de excluir a entidade ***");
+	}
+
+	@PreRemove
+	public void antesDeRemover() {
+		if (Users.getCurrent() != null) 
+			System.out.println("*** Método executado antes de remover a entidade ***");
+	}
+	
 	public String getCodigoCbo() {
 		return codigoCbo;
 	}
