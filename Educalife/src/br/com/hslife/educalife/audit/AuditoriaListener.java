afd4a1a1f8ca6536d1d8efd817210f9be825650f
Hércules S. S. José <hercules@tic.ufrj.br>
2020-12-23 13:24:23 -0300

    Valor padrão para sessões sem usuário.

--- Educalife/src/br/com/hslife/educalife/audit/AuditoriaListener.java
+++ Educalife/src/br/com/hslife/educalife/audit/AuditoriaListener.java
@@ -10,8 +10,8 @@ public class AuditoriaListener implements RevisionListener {
 	@Override
 	public void newRevision(Object userRevision) {
 		AuditoriaEntity auditoria = (AuditoriaEntity) userRevision;
-		auditoria.setUsuario(Users.getCurrent());
-		auditoria.setEnderecoIp(Users.getCurrentIP());
+		auditoria.setUsuario(Users.getCurrent() == null ? "INTERNAL": Users.getCurrent());
+		auditoria.setEnderecoIp(Users.getCurrentIP() == null ? "127.0.0.1" : Users.getCurrentIP());
 		auditoria.setDataAlteracao(Calendar.getInstance(TimeZone.getTimeZone("UTC")).getTime());		
 	}
 }
