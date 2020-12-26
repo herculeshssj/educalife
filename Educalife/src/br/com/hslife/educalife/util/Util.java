7c46d749dd75003becb85b4114ddd4a4e54739c8
Hércules S. S. José <hercules@tic.ufrj.br>
2020-12-25 22:48:22 -0300

    Inclusão do método SHA256.

--- Educalife/src/br/com/hslife/educalife/util/Util.java
+++ Educalife/src/br/com/hslife/educalife/util/Util.java
@@ -1,5 +1,8 @@
 package br.com.hslife.educalife.util;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 import java.util.UUID;
 
@@ -11,5 +14,24 @@ public class Util {
     
     public static int getRandomInt(int limit) {
         return new Random().nextInt(limit);
+    }
+
+    /*
+	 * Retorna o texto criptografado em SHA-256
+	 */
+	private static String SHA256(String texto) {
+        String sen = "";
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+            BigInteger hash = new BigInteger(1, md.digest(texto.getBytes()));
+            sen = hash.toString(16);
+        } catch (NullPointerException e) {
+        	e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        
+        return sen;
     }
 }
