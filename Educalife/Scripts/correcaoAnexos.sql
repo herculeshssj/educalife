cc8f3baafd9b2f2ea8d95e4fd1cb1d75c1e34f6f
Hércules S. S. José <hercules@tic.ufrj.br>
2020-12-27 19:45:03 -0300

    Removido os dados de exemplo em CSV e SQL.

--- Educalife/Scripts/correcaoAnexos.sql
+++
@@ -1,4 +0,0 @@
-/* Script para corrigir o problema com o upload dos anexos */
-
---- Após a criação das tabelas na base, rode o comando abaixo logado com o usuário educalife:
-alter table OXFILES alter column [data] varbinary(max);
--- Educalife/Scripts/rascunhoCargo.sql
+++
@@ -1,6 +0,0 @@
-use Educalife;
-
-select * from cargo order by denominacao asc;
-
-select * from colaborador;
-select * from audit.cargo_AUD;
