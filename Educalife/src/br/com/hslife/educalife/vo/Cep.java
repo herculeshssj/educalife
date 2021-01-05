ac7072f372ee3d2d9cf1daf7b1533f704c063485
Hércules S. S. José <hercules_hssj@outlook.com>
2021-01-04 22:59:01 -0300

    Implementado a consulta de CEP.

---
+++ Educalife/src/br/com/hslife/educalife/vo/Cep.java
@@ -0,0 +1,68 @@
+package br.com.hslife.educalife.vo;
+
+public class Cep {
+
+    private Long id;
+    private String tipoLogradouro;
+    private String logradouro;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private Integer cep;
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public Integer getCep() {
+        return cep;
+    }
+
+    public void setCep(Integer cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getTipoLogradouro() {
+        return tipoLogradouro;
+    }
+
+    public void setTipoLogradouro(String tipoLogradouro) {
+        this.tipoLogradouro = tipoLogradouro;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
