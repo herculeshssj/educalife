fed856ac5517f635b3c4163155ca3a703298ca44
Hércules S. S. José <hercules@tic.ufrj.br>
2021-02-13 20:25:33 -0300

    Salvando o rascunho.

--- Educalife/Scripts/rascunhoEquipe.sql
+++ Educalife/Scripts/rascunhoEquipe.sql
@@ -5,35 +5,7 @@ select * from turma_equipe te;
 select * from equipe e ;
 
 
-INSERT INTO public.turma_equipe
-(turma_id, funcao_equipe, ministra_aula, id_pessoa_fisica)
-VALUES('2c9c8081743156470174315d94ae0012', 'COORDENADOR', true, '2c9640817425dfa9017425e604e20003');
-INSERT INTO public.turma_equipe
-(turma_id, funcao_equipe, ministra_aula, id_pessoa_fisica)
-VALUES('2c9c8081743156470174315d94ae0012', 'INSTRUTOR', true, '2c9640817425dfa9017425e604e20005');
-INSERT INTO public.turma_equipe
-(turma_id, funcao_equipe, ministra_aula, id_pessoa_fisica)
-VALUES('2c9c8081743156470174315f9e1a0017', 'COORDENADOR', true, '2c9640817425dfa9017425e604e20003');
-INSERT INTO public.turma_equipe
-(turma_id, funcao_equipe, ministra_aula, id_pessoa_fisica)
-VALUES('2c9c8081743156470174315f9e1a0017', 'INSTRUTOR', true, '2c9640817425dfa9017425e604e20005');
-INSERT INTO public.turma_equipe
-(turma_id, funcao_equipe, ministra_aula, id_pessoa_fisica)
-VALUES('2c9c808174315647017431618823001d', 'COORDENADOR', true, '2c9640817425dfa9017425e604e20004');
-INSERT INTO public.turma_equipe
-(turma_id, funcao_equipe, ministra_aula, id_pessoa_fisica)
-VALUES('2c9c808174315647017431618823001d', 'TUTOR', false, '2c9640817425dfa9017425e604e20005');
-INSERT INTO public.turma_equipe
-(turma_id, funcao_equipe, ministra_aula, id_pessoa_fisica)
-VALUES('2c9c808174315647017431638cc40025', 'COORDENADOR', false, '2c9640817425dfa9017425e604e20004');
-INSERT INTO public.turma_equipe
-(turma_id, funcao_equipe, ministra_aula, id_pessoa_fisica)
-VALUES('2c9c808174315647017431638cc40025', 'INSTRUTOR', true, '2c9640817425dfa9017425e604e20005');
-
-select * from turma where id = '2c9c8081743156470174315d94ae0012';
-select * from pessoa_fisica pf where pf.id = '2c9640817425dfa9017425e604e20003';
-
-
+/*
 Equipe
 - nome
 - codigo
@@ -55,17 +27,21 @@ Turma
 
 Departamento
 - equipe
-
+*/
 
 select * from funcao_membro_equipe fme;
 -- update funcao_membro_equipe set lideranca = false;
 
 
-select * from membro_equipe me ;
+select * from membro_equipe me 
+inner join pessoa_fisica pf on pf.id = me.id_pessoa_fisica ;
 
 select * from equipe;
-
+select * from membro_equipe;
 
 -- alter table equipe_membro_equipe drop constraint uk_ryaktok6p9myr21u8ymwk0kgm;
 
 -- ALTER TABLE public.equipe_membro_equipe ADD CONSTRAINT uk_ryaktok6p9myr21u8ymwk0kgm UNIQUE (membrosequipe_id, equipe_id);
+
+
+
