f18d6e5234479c13caa118aa85b7ab2f10d43e38
Hércules S. S. José <hercules@tic.ufrj.br>
2021-03-25 08:02:24 -0300

    Inclusão automática do usuário admin

--- Educalife/src/br/com/hslife/educalife/sample/Sample.java
+++ Educalife/src/br/com/hslife/educalife/sample/Sample.java
@@ -1,7 +1,15 @@
 package br.com.hslife.educalife.sample;
 
+import java.util.Optional;
+
+import org.openxava.jpa.XPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import br.com.hslife.educalife.dao.UsuarioDAO;
+import br.com.hslife.educalife.model.PapelUsuario;
+import br.com.hslife.educalife.model.Usuario;
+import br.com.hslife.educalife.patterns.Builder;
 
 @Component
 public class Sample {
@@ -23,11 +31,15 @@ public class Sample {
 
     @Autowired
     SampleColaborador sampleColaborador;
+
+    @Autowired
+    UsuarioDAO usuarioDAO;
     
     /*
         Invoca todos os métodos que populam a base
     */
     public void populaBase() {
+        verificaPresencaUsuarioAdmin();
         //SampleCargo.populaCargo();
         samplePessoaFisica.populaBase();
         samplePessoaJuridica.populaBase();
@@ -35,5 +47,30 @@ public class Sample {
         sampleDocumentoIdentidade.populaBase();
         sampleUsuario.populaBase();
         sampleColaborador.populaBase();
+    }
+
+    private void verificaPresencaUsuarioAdmin() {
+        // Verifica se o usuário admin está cadastrado na base.
+        // Caso contrário, realiza o cadastro dele com suas
+        // devidas permissões.
+        Optional<Usuario> usuarioOptional = usuarioDAO.buscarPorLogin("admin");
+        if (!usuarioOptional.isPresent()) {
+            // Cadastra o usuário admin
+            try {
+                Builder<Usuario> usuarioBuilder = new Builder<Usuario>(new Usuario());
+                Usuario usuario = usuarioBuilder
+                    .set("login", "admin")
+                    .set("senha", "admin")
+                    .set("ativo", Boolean.TRUE)                
+                    .build();
+
+                // Salva o usuário
+                XPersistence.getManager().persist(usuario);
+                XPersistence.commit();
+            } catch (Exception e) {
+                XPersistence.rollback();
+			    e.printStackTrace();
+            }
+        }
     }
 }
